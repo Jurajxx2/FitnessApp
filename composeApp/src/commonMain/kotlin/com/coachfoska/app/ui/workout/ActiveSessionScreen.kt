ba3d8f1fab959bc638ca91b6d5c51c11fab171b3
juraj <juraj.trusina@gmail.com>
2026-05-27 01:33:48 +0200

    fix(workout): remove unused Surface import from ActiveSessionScreen

--- composeApp/src/commonMain/kotlin/com/coachfoska/app/ui/workout/ActiveSessionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/coachfoska/app/ui/workout/ActiveSessionScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SuggestionChip
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
